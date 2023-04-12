10bbb6bfb00f6aa7c0aed0c99a29bc27bc80b642
Naoko Sugawara <infoseek122@gmail.com>
2023-04-12 15:22:13 -0400

    adding User model

--- joshua_naoko_project/app/controllers/users_controller.rb
+++ joshua_naoko_project/app/controllers/users_controller.rb
@@ -1,12 +1,16 @@
 class UsersController < ApplicationController
 
     def index
-        render plain: "I'm in the index action!"
+        @users = User.all
+        render json: @users
     end
 
     def create
-        render json: params
-    end
+        user = User.new(params.require(:user).permit(:user_attributes_here))
+        # replace the `user_attributes_here` with the actual attribute keys
+        user.save!
+        render json: user
+      end
 
     def show
         render json: params
